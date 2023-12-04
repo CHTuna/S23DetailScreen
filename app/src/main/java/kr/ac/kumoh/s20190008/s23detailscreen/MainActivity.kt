9b3c65f920820756f86974975962362047dffce9
EIVE <45841115+CHTuna@users.noreply.github.com>
2023-12-04 14:47:55 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/kumoh/s20190008/s23detailscreen/MainActivity.kt
@@ -0,0 +1,35 @@
+package kr.ac.kumoh.s20190008.s23detailscreen
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import kr.ac.kumoh.s20190008.s23detailscreen.ui.theme.S23DetailScreenTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainScreen()
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    S23DetailScreenTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            SongApp()
+        }
+    }
+}
